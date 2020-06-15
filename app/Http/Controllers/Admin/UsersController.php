72d4eb0d501b283682af90136386db06dba9f9fa
Eliza <itzjusteliza@gmail.com>
2020-06-15 19:01:14 +0200

    Authentication in progress, added user management

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -15,40 +15,11 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        return 'User index page!!';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // DELETED CREATE AND RESTORE FUNCTIONS BECAUSE NOT NEEDED
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
