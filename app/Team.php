0d3052e6c2ec1979f86f8ac43ccda9b36d3e676d
Edwin Berg <e.berg117@gmail.com>
2020-06-17 15:28:25 +0200

    updated some stuff

--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'team_name', 'activity_id'
+    ];
+
+
+    /**
+     * Relations with Activity
+     * 
+     * @return \App\Activity
+     */
+    public function activity() {
+        return $this->belongsTo('App\Activity');
+    }
 }
