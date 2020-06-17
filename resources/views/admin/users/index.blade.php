ec20f9384de994956e8eda681e9f0d637ea481b3
Edwin Berg <e.berg117@gmail.com>
2020-06-17 15:29:40 +0200

    Updated some stuff

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="float-left ml-2 mt-4">
+    <div class="card p-3" style="width: 18rem;">
+        <a href="{{ route('admin.users.index') }}"> Visa alla medlemmar</a>
+        <a href="{{ route('teams.index') }}">Lag</a>
+    </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,16 +14,12 @@
                 <div class="card-header">Medlemmar</div>
                 <div class="card-body">
 
-                    <!-- HÄMTA DATABAS MEDLEMMARNA HÄR -->
                     <table class="table table-hover">
                         <thead class="thead-light">
                             <tr>
                                 <th scope="col">Antal</th>
                                 <th scope="col">Namn</th>
                                 <th scope="col">Email</th>
-                                <th scope="col">Aktivitet</th>
-                                <th scope="col">Lag</th>
-                                <th scope="col">Medlemsavgift</th>
                             </tr>
                         </thead>
                         @foreach($users as $key=>$user)
@@ -29,7 +31,6 @@
                             </tr>
                         </tbody>
                         @endforeach
-                        <h6><u>Summan av medlemsavgift:</u> {{}}</h6>
                     </table>
                 </div>
             </div>
