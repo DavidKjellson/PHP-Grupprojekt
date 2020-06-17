c3dfedaefd5c504ac6e9d7e34a4806c4d5b2e44e
Edwin Berg <e.berg117@gmail.com>
2020-06-17 20:43:20 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-// Route::get('/admin/teams', function () {
-//     return view('admin/show_teams.teams');
-// });
-
-
 // Strukturerar routerna med prefix och namn, OBS! glöm inte ändra i app.blade.php 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
     Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
@@ -32,3 +26,5 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
 
 
 Route::resource('/teams', 'TeamsController');
+
+Route::patch('teams/{team}/detach/{id}', 'TeamsController@detach')->name('teams.detach');
