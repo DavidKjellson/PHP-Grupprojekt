27fdb9f0adea4d442412d18b964898312a065853
Edwin Berg <e.berg117@gmail.com>
2020-06-17 20:26:50 +0200

    changed the name of the table within the db

---
+++ database/migrations/2020_06_15_000000_create_team_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            //detta table används för att koppla samman användarna (users) med aktiviteterna
+            $table->id('team_id');
+            $table->string('user_id'); //användarens id (från user table)
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_user');
+    }
+}
